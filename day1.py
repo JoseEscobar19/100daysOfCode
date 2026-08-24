97deaa4475895b1309514259804c6a56e51019ae
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-24 03:14:36 -0400

    day1 practice
    Print function, variable declaring data types, notes

---
+++ day1.py
@@ -0,0 +1,28 @@
+##in the terminal to open a file in vs code: code [name of the file]
+
+## day1 practice print(" ")
+print("hello world")
+
+## declaring values stored in variables
+
+x = 12.2
+y = 7
+z = 9.0
+
+print(x)
+print(y)
+print(z)
+
+
+##primitive data types 
+# python built function to check the type of data = type(var)
+
+name = "jose"
+age = 26
+cash = 100.50
+thereyet = False
+
+print("data type of name is", type(name)) 
+print("data type of name is", type(age)) 
+print("data type of name is", type(cash)) 
+print("data type of name is", type(thereyet)) 
