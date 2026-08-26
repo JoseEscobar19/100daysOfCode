bfb185524e2a4498ca42ba0ffa49c8d597a960e8
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-26 04:16:59 -0400

    Enhance day3.py with loop explanations and examples
    Added explanations for for and while loops, including examples of break and continue statements.

--- day3.py
+++ day3.py
@@ -37,3 +37,51 @@ if x > 1 and x < 100:
     print("less han 100")
 print("all done")
 
+
+#for -- loops are definite iteration. You use them when you know in advance—or can easily
+#measure—how many items or iterations you are dealing with (like repeating 10 times or
+#looping through a list of 5 items).
+
+#while -- loops are indefinite iteration. You use them when the loop depends on a
+#dynamic condition rather than a count, meaning it will run continuously until a specific
+#condition changes (like waiting for user input or waiting for a game-over flag).
+## (WHILE a condition is met, do this task)
+
+#normally a while will be used with a Iteration Variable
+# the iteration variable is a variable that is set to change in value everytime a loop is repeated once; in this case n = 5 and will chnage
+
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("blastoff!")
+print(n)
+
+#output: 5,4,3,2,1,blastoff!,0
+
+## break statement -- Terminates loop entirely when encountered
+#in the example below: itll ask for someones name until they enter a name, if nothing is typed itll keep on asking over again
+
+while True:
+    name = input("enter your name")
+    if name != "":          #if name doesnt equal a set of quotes(if they dont type anything)
+        break
+
+## continue - skips to the next iteration of the loop
+#in the example below, want to display a number without the dahses - 
+
+phone_num = "808-123-321"
+
+for i in phone_num:
+    if i == "-":        #if i is equal to a dash 
+        continue        # then it skips that ieration of the loop
+    print(i)
+
+
+### input() - funtion is a built in function that instructs python to pause and read data from user
+## data passed in will always be a strinng; even if a num int is enttered, itll be a string data type
+##  variable = input(promt asked)
+# the code will prompt user to enter their name then store the var in the varibale callled name
+nombre = input("what is your name?")
+
+print(nombre)   #itll print name enterred
