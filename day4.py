1c81135d957210187c1a6b9f0218b60d27d5a3e6
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-27 00:20:33 -0400

    Change loop increment and modify print logic
    Refactor loop to increment x by 1 and adjust print statements for clarity.

--- day4.py
+++ day4.py
@@ -58,14 +58,28 @@ else:
 x = 0
 
 while x != 10:
-    x = x + 0.1
-    if x > 1:
-        break
+    x = x + 1
     if x < 5:
         print(x)
     elif x == 6:
-        break
+        print(x)
+        continue
     elif x >= 5 and x <= 8:
         print("x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is:", x)
     else:
         print("x is bigger than 8. It is:", x)
+
+#output
+'''
+1
+2
+3
+4
+x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is: 5
+6
+x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is: 7
+x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is: 8
+x is bigger than 8. It is: 9
+x is bigger than 8. It is: 10
+'''
+
