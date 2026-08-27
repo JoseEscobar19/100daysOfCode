a03d0a7279969691ad2df190950c8f0033acb893
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-26 23:54:20 -0400

    Refactor price checking logic with nested ifs

--- day4.py
+++ day4.py
@@ -23,3 +23,29 @@ elif gradeToTest >= 50:
    print("D")
 else:
     print("F")
+##########################
+
+#practice nested if
+priceIsRight = 15
+
+if priceIsRight:
+    print("Price is too low!")
+    if priceIsRight:
+        print("Price is almost there!")
+        if priceIsRight:
+            print("Price is exactly that!")
+        if priceIsRight:
+            print("Price is too high!")
+
+#the code above is turned to code below
+
+priceIsRight = 7
+
+if priceIsRight < 5:
+    print("Price is too low!")
+elif priceIsRight >= 5 and priceIsRight <= 9:
+    print("Price is almost there!")
+elif priceIsRight == 10:
+    print("Price is exactly that!")
+else:
+    print("Price is too high!")
