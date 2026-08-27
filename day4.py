5e572e940d352e15f8fcfb43ad1968e826708b5b
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-26 23:56:24 -0400

    Add while loop practice with conditional checks

--- day4.py
+++ day4.py
@@ -49,3 +49,23 @@ elif priceIsRight == 10:
     print("Price is exactly that!")
 else:
     print("Price is too high!")
+
+
+###############################
+
+## practice while loops
+
+x = 0
+
+while x != 10:
+    x = x + 0.1
+    if x > 1:
+        break
+    if x < 5:
+        print(x)
+    elif x == 6:
+        break
+    elif x >= 5 and x <= 8:
+        print("x is bigger then or equal to 5, and less then or equal to 8, but not 6. It is:", x)
+    else:
+        print("x is bigger than 8. It is:", x)
