be388326f352a642744794d6b57f2762c8bc87b4
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-27 23:40:00 -0400

    Add various range and iterable examples in day6.py

---
+++ day6.py
@@ -0,0 +1,17 @@
+for i in range(0, 5, 1):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(5, 0, -1):
+    print(i)
+
+
+# range() style
+for i in range(0, 5, 1):
+    print(i)
+
+# iterable style
+for x in "string":
+    print(x)
