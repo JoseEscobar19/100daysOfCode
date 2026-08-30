735cf3ba036b95849e9b0cd4431ecc972fad474c
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-08-29 23:24:39 -0400

    Enhance day8.ps1 with comments on variables and commands
    Added comments explaining constant variables and running multiple commands.

--- day8.ps1
+++ day8.ps1
@@ -21,3 +21,21 @@ $MaximumHistoryCount
 
 $MaximumHistoryCount = 200
 
+#to set a variable to be constant and anti change you do the 
+Set-Variable -Name 'color' -Value 'green' -Option Constant
+#this tells the variable named color, that has value green, to be a contant and anti chnage
+$color
+#if you try to change it to blue from the current green
+$color = 'blue'
+#itll give an error
+
+
+# to run two commands at the same time you can use the ; semicolo
+#this allows for the console to run two commands
+
+ping.exe 127.0.0.1 -n 1 ; $LastExitCode
+
+#objects in powershell
+
+
+
