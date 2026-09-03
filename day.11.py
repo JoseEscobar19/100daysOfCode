4f8f7674561a95aff960240f83806c55402e65ac
Jose Sejas Escobar <153658903+JoseEscobar19@users.noreply.github.com>
2026-09-02 23:58:32 -0400

    Add for loop examples and control statements
    Added examples of for loops using range and iteration, including break and continue statements.

---
+++ day.11.py
@@ -0,0 +1,31 @@
+#using for in range()
+for i in range(0,5,1):
+    print(i)
+
+#using for in iteration
+
+for i in "jose":
+    print(i)
+
+me = "Pepe"
+
+for i in me:
+    print(i)
+
+
+'''For loop control'''
+'''break = stop all code iterations and loop completely
+ conitnue  = atop current itertion'''
+
+for i in "string":
+    if i == 'n':
+        break
+    print(i)
+
+for i in "strings":
+    if i == 'n':
+        continue
+    print(i)
+
+"""Excerise: For loops using rnage()"""
+
